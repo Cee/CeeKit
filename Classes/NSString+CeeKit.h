a3cbb2a9db5491dbec60629d9a187f4008341705
Cee <cee@chu2byo.com>
2015-02-17 22:48:31 +0800

    Add NSString Category

---
+++ Classes/NSString+CeeKit.h
@@ -0,0 +1,14 @@
+//
+//  NSString+CeeKit.h
+//  CeeKit
+//
+//  Created by Cee on 17/02/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (CeeKit)
+- (NSString *)URLEncodedString;
+- (NSString *)URLDecodedString;
+@end
---
+++ Classes/NSString+CeeKit.m
@@ -0,0 +1,34 @@
+//
+//  NSString+CeeKit.m
+//  CeeKit
+//
+//  Created by Cee on 17/02/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import "NSString+CeeKit.h"
+
+@implementation NSString (CeeKit)
+
+- (NSString *)URLEncodedString
+{
+    NSString *encodedString = (NSString *)
+    CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+                                                              (CFStringRef)self,
+                                                              (CFStringRef)@"!$&'()*+,-./:;=?@_~%#[]",
+                                                              NULL,
+                                                              kCFStringEncodingUTF8));
+    return encodedString;
+}
+
+- (NSString *)URLDecodedString
+{
+    NSString *decodedString = (NSString *)
+    CFBridgingRelease(CFURLCreateStringByReplacingPercentEscapesUsingEncoding(kCFAllocatorDefault,
+                                                                              (CFStringRef)self,
+                                                                              CFSTR(""),
+                                                                              kCFStringEncodingUTF8));
+    return decodedString;
+}
+
+@end
