e881820522d3519c2c187a08b80194ea5deda715
Cee <cee@chu2byo.com>
2015-02-19 23:00:17 +0800

    Add UIImage Category

---
+++ Classes/UIImage+CeeKit.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+CeeKit.h
+//  CeeKit
+//
+//  Created by Cee on 19/02/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (CeeKit)
+
+@end
---
+++ Classes/UIImage+CeeKit.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+CeeKit.m
+//  CeeKit
+//
+//  Created by Cee on 19/02/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import "UIImage+CeeKit.h"
+
+@implementation UIImage (CeeKit)
+
+@end
